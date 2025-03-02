7a7fd4f7d293d4c1ea35b0faf690546396f6e7ba
David Reichardt <davereichardt@gmail.com>
2025-03-02 00:47:53 -0500

    restarting with frontend only

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'auth': 'http://localhost:3001',
-		}
-	}
+	plugins: [sveltekit()]
 });
